8b78931e67bb3f4e97f40e52ed3eef58959d4eb8
chanan-hash <67066282+chanan-hash@users.noreply.github.com>
2024-02-26 18:50:36 +0200

    Add files via upload

---
+++ comunication ex3/RUDP_Sender.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h> // For the memset function
+#include <arpa/inet.h> // For the in_addr structure and the inet_pton function
+#include <sys/socket.h> // For the socket function
+#include <unistd.h> // For the close function
+#include <sys/time.h> // For tv struct
+
+
