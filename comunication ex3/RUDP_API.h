5e59e937028b0b341d1e9be3055a7936e82f0641
chanan-hash <67066282+chanan-hash@users.noreply.github.com>
2024-03-07 17:47:52 +0200

    Updating and improving

--- comunication ex3/RUDP_API.h
+++ comunication ex3/RUDP_API.h
@@ -2,11 +2,13 @@
 
 #ifndef RUDP_API_H
 #define RUDP_API_H
-#define MAX_SIZE 60000 // will be for max size of data each time
+#define MAX_SIZE 60000  // will be for max size of data each time
 
-#include <stdio.h>
+#include <stdbool.h>  // For the boolean signs flags
 #include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 // Creating the socket, return -1 if fails
 int rudp_socket();
@@ -23,34 +25,43 @@ int rudp_receive(int socket, char **buffer, int *length);
 int rudp_close(int socket);
 
 /**** More help functions ****/
-// The idea to make UDP reliable, is to make it like TCP protocol, adding like listen and bind functions
+// The idea to make UDP reliable, is to make it like TCP protocol, adding like
+// listen and bind functions
 
-// Opening the connection itself between with the socket and the given ip and port
-// This is the 2 handshake, returning 1 if succedd 0 if fails
+// Opening the connection itself between with the socket and the given ip and
+// port This is the 2 handshake, returning 1 if succedd 0 if fails
 int rudp_connect(int socket, const char *ip, int port);
 
 // Listening for incoming connection, the other side of the 2 handshake
 // returning 1 if succedd 0 if fails
 int rudp_get_con(int socket, int port);
 
-/*****Helpers structs for checking the data transfer and for the protocol itself ****/
+/*****Helpers structs for checking the data transfer and for the protocol itself
+ * ****/
 
-// We're using here bit field like SYN flag will hae 1 for true and 0 for false, and that will create our flage
-struct RUDP_flag
-{
-    unsigned int SYN : 1; // Synchronize flag to know if someone want to establish connection 
-    unsigned int ACK : 1; // Acknowledge this for ensure that we've got the whole data
-    unsigned int DATA : 1; // To know if the packet is carring a data payload
-    unsigned int FIN : 1; // To know if we've finished to send everything and can finish the connection
+// We're using here boolean signs flag, we'll have 1 for true and 0 for false,
+// That will help us to know for each packet what's the state of the connection
+
+struct Flags {
+  bool isSynchronized;  // Synchronize flag to know if someone want to establish
+                        // connection
+
+  bool finishFlag;  // To know if we've finished to send everything and can
+                    // finish the connection
+
+  bool acknowledgeFlag;  // Acknowledge this for ensure that we've got the whole
+                         // data
+
+  bool isDataPacket;  // To know if the packet is carrying a data payload
 };
 
-typedef struct _RUDP
-{
-    struct RUDP_flag flags;
-    int sequalNum;
-    int checksum;
-    int dataLength;
-    char data[MAX_SIZE];
+// This struct will represent a UDP packet, with the flags and the data
+typedef struct _RUDP {
+  struct Flags flags;
+  int sequalNum;
+  int checksum;
+  int dataLength;
+  char data[MAX_SIZE];
 } RUDP;
 
 #endif
